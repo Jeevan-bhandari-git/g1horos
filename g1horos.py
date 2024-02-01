30dd065a2cab3babb16c6d9299968063d1935adc
Jeevan Bhandari <60292222+Jeevan-bhandari-git@users.noreply.github.com>
2024-02-01 17:54:33 +0530

    Update g1horos.py

--- g1horos.py
+++ g1horos.py
@@ -102,5 +102,5 @@ def g1hor():
     return jsonify(content=content)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
     #app.run(debug=True)
